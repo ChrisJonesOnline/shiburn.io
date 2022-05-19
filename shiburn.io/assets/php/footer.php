bb9fc21d581e34c1a97102df8ed918289cfb027d
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2022-05-19 01:05:17 -0700

    Add files via upload

--- shiburn.io/assets/php/footer.php
+++ shiburn.io/assets/php/footer.php
@@ -6,6 +6,8 @@
         &copy; <?php echo date("Y"); ?> shiburn.io | All Rights Reserved.
         <br />
         All other trademarks and images are property of their respective owners.
+		<br />
+		Powered by: <a href="https://chrisjones.online">chrisjones.online</a>
         </p>
         </center>
         <br />
