0fb0baa7a312374c58667073bcee4dbea85a4701
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2022-06-28 19:35:11 +0000

    Delete footer.php

--- shiburn.io/assets/php/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class='footer'>
-&copy; <?php echo date("Y"); ?> shiburn.io | All Rights Reserved.
-        <br />
-        All other trademarks and images are property of their respective owners.
-</footer>
