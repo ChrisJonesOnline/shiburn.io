cb7d7561620c893d0fb061021125f6bce869b682
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2022-07-11 14:11:58 -0700

    Update index.php

--- shiburn.io/index.php
+++ shiburn.io/index.php
@@ -56,7 +56,7 @@
         <input class='search' type='text' placeholder='Search wallet address...' autocomplete='off'></input>
         <style class='search_style'></style>
         </p>
-       </header>
+    </header>
     ";
 ?>
 
@@ -65,23 +65,23 @@
     foreach (array_reverse($jsonArray['result']) as $result) {
 
         $date = date('l, m/d/Y, H:i:s ',$result['timeStamp']);
-		$from = $result['from'];
-		$burned = number_format($result['value'] / 1000000000000000000);
-		$usdValue = number_format(($result['value'] / 1000000000000000000) * $marketPrice);
-		$txHash = $result['hash'];
+        $from = $result['from'];
+        $burned = number_format($result['value'] / 1000000000000000000);
+        $usdValue = number_format(($result['value'] / 1000000000000000000) * $marketPrice);
+        $txHash = $result['hash'];
 
-		echo "
-        <div class='searchable' data-index='$from'>
-            <b>Date: </b> $date
-            <br />
-            <b>From: </b> <a href='https://etherscan.io/tx/$txHash' target='_blank'>$from</a>
-            <br />
-            <b>Amount: </b> $burned
-            <br />
-            <b>USD Value: </b>≈ $$usdValue
-            <br />
-        </div>
-		";
+            echo "
+            <div class='searchable' data-index='$from'>
+                <b>Date: </b> $date
+                <br />
+                <b>From: </b> <a href='https://etherscan.io/tx/$txHash' target='_blank'>$from</a>
+                <br />
+                <b>Amount: </b> $burned
+                <br />
+                <b>USD Value: </b>≈ $$usdValue
+                <br />
+            </div>
+            ";
 	}
 ?>
 </div>
