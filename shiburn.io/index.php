5bcd87ae1a9236474fd75fa9a29673ac4ff8216f
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2022-07-13 15:09:43 -0700

    Update index.php

--- shiburn.io/index.php
+++ shiburn.io/index.php
@@ -21,7 +21,7 @@
     $result = $fullArray[0];
     $value = $result['value'] / 1000000000000000000;
 
-    $marketCap = '10589909087';
+    $marketCap = '10399910578';
     $totalSupply = '999992170305138';
     $totalBurned = $value;
     $totalSupplyLeft = ($totalSupply - $value);
