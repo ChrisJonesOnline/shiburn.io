a321d21d9868ddf29a3f675a2291b5d8d86aa7ab
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2022-08-02 18:48:02 -0700

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     $result = $fullArray[0];
     $value = $result['value'] / 1000000000000000000;
 
-    $marketCap = '11809897403.66';
+    $marketCap = '11719897927.11';
     $totalSupply = '999991312756761.40';
     $totalBurned = $value;
     $totalSupplyLeft = ($totalSupply - $value);
